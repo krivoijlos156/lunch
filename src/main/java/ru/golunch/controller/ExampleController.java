8f436127888387e9bc3e4ec32a7b5b6e1d1bb94d
krivoijlos156 <krivoijlos@gmail.com>
2020-09-09 01:01:18 +0300

    add spring mvc add testController

---
+++ src/main/java/ru/golunch/controller/ExampleController.java
@@ -0,0 +1,16 @@
+package ru.golunch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+
+
+    @GetMapping("/example")
+    public String example() {
+
+        return "";
+    }
+}
---
+++ src/main/java/ru/golunch/controller/WebMvcConfig.java
@@ -0,0 +1,9 @@
+package ru.golunch.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig {
+}
